d7de2da53a9e7e24380c3b7225c15c131f0601c7
CGPxy <52651150+CGPxy@users.noreply.github.com>
2023-04-18 14:28:58 +0800

    Update compare_function.py

--- compare_function.py
+++ compare_function.py
@@ -144,105 +144,118 @@ HDlist = []
 ASSDlist = []
 ABDlist = []
 
+def compare(args):
+    savepath = args["Savepath"]
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+    excle1 = xlsxwriter.Workbook(savepath + "NUnet11.xlsx")
+    worksheet = excle1.add_worksheet()
+    worksheet.write(0,0,"image_name")
+    worksheet.write(0,1,"PA")
+    worksheet.write(0,2,"IoU")
+    worksheet.write(0,3,"Precision")
+    worksheet.write(0,4,"Recall")
+    worksheet.write(0,5,"Specificity")
+    worksheet.write(0,6,"F1_socre")
+    worksheet.write(0,7,"Dice")
+    worksheet.write(0,8,"HD")
+    worksheet.write(0,9,"ASSD")
+    worksheet.write(0,10,"ABD")
 
-savepath = '/media/dy/Data_2T/CGP/Unet_Segnet/NU_net/result/excel/unet_MDS_MOU/BUSI/'
-if not os.path.exists(savepath):
-    os.makedirs(savepath)
-excle1 = xlsxwriter.Workbook(savepath + "NUnet11.xlsx")
-worksheet = excle1.add_worksheet()
-worksheet.write(0,0,"image_name")
-worksheet.write(0,1,"PA")
-worksheet.write(0,2,"IoU")
-worksheet.write(0,3,"Precision")
-worksheet.write(0,4,"Recall")
-worksheet.write(0,5,"Specificity")
-worksheet.write(0,6,"F1_socre")
-worksheet.write(0,7,"Dice")
-worksheet.write(0,8,"HD")
-worksheet.write(0,9,"ASSD")
-worksheet.write(0,10,"ABD")
+    # predict mask
+    image_path = args["Imagepath"]
 
-# predict mask
-image_path = '/media/dy/Data_2T/CGP/Unet_Segnet/data/Breast/BUSI/new-BUSI/1/Test_images/labels/384/'
+    # Ground-truth mask
+    mask_path = args["Labelpath"]
 
-# Ground-turth mask
-mask_path = '/media/dy/Data_2T/CGP/Unet_Segnet/NU_net/result/mask/unet_MDS_MOU/BUSI/1/'
-
-filelist = os.listdir(mask_path)
-i = 0
-for item in filelist:
-    print(item)
-    i = i+1
-    image1 = cv2.imread(image_path + item, cv2.IMREAD_GRAYSCALE)
-    mask1 = cv2.imread(mask_path + item, cv2.IMREAD_GRAYSCALE)
-    image=tf.cast(image1,tf.float32)
-    mask=tf.cast(mask1,tf.float32)
-
-    Himage = cv2.imread(image_path + item)
-    Hmask = cv2.imread(mask_path + item)
-    hd = HD_base(Himage, Hmask)
-    assd = ASSD(Himage, Hmask)
-    abd = ABD(Himage, Hmask)
-
-    pa = PA(mask, image)
-    iou = IoU(mask, image)
-    precision = Precision(mask, image)
-    recall = Recall(mask, image)
-    specificity = Specificity(mask, image)
-    f1_score = F1_socre(mask, image)
-    dice = Dice(mask, image)
-
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        PAlist.append(sess.run(pa))
-        IoUlist.append(sess.run(iou))
-        Precisionlist.append(sess.run(precision))
-        Recalllist.append(sess.run(recall))
-        Specificitylist.append(sess.run(specificity))
-        F1_socrelist.append(sess.run(f1_score))
-        Dicelist.append(sess.run(dice))
-        HDlist.append(hd)
-        ASSDlist.append(assd)
-        ABDlist.append(abd)
-
+    filelist = os.listdir(mask_path)
+    i = 0
+    for item in filelist:
         print(item)
-        print("PA:%f   IoU:%f  Precision:%f  Recall:%f  Specificity:%f   F1_socre:%f    Dice:%f" 
-            %(sess.run(pa), sess.run(iou), sess.run(precision), sess.run(recall), sess.run(specificity),
-             sess.run(f1_score), sess.run(dice)))
+        i = i+1
+        image1 = cv2.imread(image_path + item, cv2.IMREAD_GRAYSCALE)
+        mask1 = cv2.imread(mask_path + item, cv2.IMREAD_GRAYSCALE)
+        image=tf.cast(image1,tf.float32)
+        mask=tf.cast(mask1,tf.float32)
 
-        worksheet.write(i,0,item)
-        worksheet.write(i,1,sess.run(pa))
-        worksheet.write(i,2,sess.run(iou))
-        worksheet.write(i,3,sess.run(precision))
-        worksheet.write(i,4,sess.run(recall))
-        worksheet.write(i,5,sess.run(specificity))
-        worksheet.write(i,6,sess.run(f1_score))
-        worksheet.write(i,7,sess.run(dice))
-        worksheet.write(i,8,hd)
-        worksheet.write(i,9,assd)
-        worksheet.write(i,10,abd)
+        Himage = cv2.imread(image_path + item)
+        Hmask = cv2.imread(mask_path + item)
+        hd = HD_base(Himage, Hmask)
+        assd = ASSD(Himage, Hmask)
+        abd = ABD(Himage, Hmask)
 
-if i != 0:
+        pa = PA(mask, image)
+        iou = IoU(mask, image)
+        precision = Precision(mask, image)
+        recall = Recall(mask, image)
+        specificity = Specificity(mask, image)
+        f1_score = F1_socre(mask, image)
+        dice = Dice(mask, image)
 
-    worksheet.write(i+2,1,sum(PAlist) / i)
-    worksheet.write(i+2,2,sum(IoUlist) / i)
-    worksheet.write(i+2,3,sum(Precisionlist) / i)
-    worksheet.write(i+2,4,sum(Recalllist) / i)
-    worksheet.write(i+2,5,sum(Specificitylist) / i)
-    worksheet.write(i+2,6,sum(F1_socrelist) / i)
-    worksheet.write(i+2,7,sum(Dicelist) / i)
-    worksheet.write(i+2,8,sum(HDlist) / i)
-    worksheet.write(i+2,9,sum(ASSDlist) / i)
-    worksheet.write(i+2,10,sum(ABDlist) / i)   
-    excle1.close()
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            PAlist.append(sess.run(pa))
+            IoUlist.append(sess.run(iou))
+            Precisionlist.append(sess.run(precision))
+            Recalllist.append(sess.run(recall))
+            Specificitylist.append(sess.run(specificity))
+            F1_socrelist.append(sess.run(f1_score))
+            Dicelist.append(sess.run(dice))
+            HDlist.append(hd)
+            ASSDlist.append(assd)
+            ABDlist.append(abd)
 
-    print("MPA:%f" % (sum(PAlist) / i))
-    print("MIoU:%f" % (sum(IoUlist) / i))
-    print("MPrecision:%f" % (sum(Precisionlist) / i))
-    print("MRecall:%f" % (sum(Recalllist) / i))
-    print("MSpecificity:%f" % (sum(Specificitylist) / i))
-    print("MF1_score:%f" % (sum(F1_socrelist) / i))
-    print("Dice:%f" % (sum(Dicelist) / i))
-    print("HD:%f" % (sum(HDlist) / i))
-    print("ASSD:%f" % (sum(ASSDlist) / i))
-    print("ABD:%f" % (sum(ABDlist) / i))
+            print(item)
+            print("PA:%f   IoU:%f  Precision:%f  Recall:%f  Specificity:%f   F1_socre:%f    Dice:%f" 
+                %(sess.run(pa), sess.run(iou), sess.run(precision), sess.run(recall), sess.run(specificity),
+                 sess.run(f1_score), sess.run(dice)))
+
+            worksheet.write(i,0,item)
+            worksheet.write(i,1,sess.run(pa))
+            worksheet.write(i,2,sess.run(iou))
+            worksheet.write(i,3,sess.run(precision))
+            worksheet.write(i,4,sess.run(recall))
+            worksheet.write(i,5,sess.run(specificity))
+            worksheet.write(i,6,sess.run(f1_score))
+            worksheet.write(i,7,sess.run(dice))
+            worksheet.write(i,8,hd)
+            worksheet.write(i,9,assd)
+            worksheet.write(i,10,abd)
+
+    if i != 0:
+
+        worksheet.write(i+2,1,sum(PAlist) / i)
+        worksheet.write(i+2,2,sum(IoUlist) / i)
+        worksheet.write(i+2,3,sum(Precisionlist) / i)
+        worksheet.write(i+2,4,sum(Recalllist) / i)
+        worksheet.write(i+2,5,sum(Specificitylist) / i)
+        worksheet.write(i+2,6,sum(F1_socrelist) / i)
+        worksheet.write(i+2,7,sum(Dicelist) / i)
+        worksheet.write(i+2,8,sum(HDlist) / i)
+        worksheet.write(i+2,9,sum(ASSDlist) / i)
+        worksheet.write(i+2,10,sum(ABDlist) / i)   
+        excle1.close()
+
+        print("MPA:%f" % (sum(PAlist) / i))
+        print("MIoU:%f" % (sum(IoUlist) / i))
+        print("MPrecision:%f" % (sum(Precisionlist) / i))
+        print("MRecall:%f" % (sum(Recalllist) / i))
+        print("MSpecificity:%f" % (sum(Specificitylist) / i))
+        print("MF1_score:%f" % (sum(F1_socrelist) / i))
+        print("Dice:%f" % (sum(Dicelist) / i))
+        print("HD:%f" % (sum(HDlist) / i))
+        print("ASSD:%f" % (sum(ASSDlist) / i))
+        print("ABD:%f" % (sum(ABDlist) / i))
+
+def args_parse():
+# construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--Imagepath", required=True, help="path of your predic mask")
+    ap.add_argument("--Labelpath", required=True, help="path of your ground-truth mask")
+    ap.add_argument("--Savepath", required=True, help="path to save your results")
+    args = vars(ap.parse_args())    
+    return args
+
+if __name__ == '__main__':
+    args = args_parse()
+    compare(args)
